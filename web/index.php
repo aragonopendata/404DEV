90025b83b187417d8c769429db9563b729acc5eb
SafeTrip <juan@esporrin.com>
2014-09-26 23:42:28 +0200

    primera maquetacion web

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+<meta charset="UTF-8">
+
+<!-- Favicon -->
+<link rel="icon" href="css/img/favicon.png">
+
+<!-- General CSS -->
+<link rel="stylesheet/less" href="css/aragomo.less">
+
+<!-- Vendor JS libraries  -->
+<script src="vendor/less/less-1.7.5.min.js"></script>
+<script src="vendor/jQuery/jquery-1.11.1.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/modernizr/modernizr.js"></script>
+
+<!-- Vendor CSS libraries -->
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap-theme.min.css">
+
+<!-- General JS -->
+<script src="js/aragomo.js"></script>
+
+<title>Aragomo</title>
+</head>
+
+<body class="aragomo">
+	<?php include 'includes/header.html';?>
+
+	
+	
+	<?php include 'includes/footer.html';?>
+</body>
+
+</html>
