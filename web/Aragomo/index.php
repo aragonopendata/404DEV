9cbe9e005fd5fdeb6be58c739ab16f10a4cb0a6b
jordivador <jordivador@gmail.com>
2014-09-27 14:30:01 +0200

    modification header index.php

--- web/Aragomo/index.php
+++ web/Aragomo/index.php
@@ -14,6 +14,8 @@ if(isset($_GET['lat'])){
 <html>
 
 <head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
 <meta charset="UTF-8">
 
 <!-- Favicon -->
