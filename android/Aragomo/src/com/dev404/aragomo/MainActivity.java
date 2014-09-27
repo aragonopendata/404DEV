d50368ac37ecaf51699d4ca10db723f5b3f992e5
SafeTrip <juan@esporrin.com>
2014-09-27 03:30:54 +0200

    actualizacion assets + android

--- android/Aragomo/src/com/dev404/aragomo/MainActivity.java
+++ android/Aragomo/src/com/dev404/aragomo/MainActivity.java
@@ -2,14 +2,21 @@ package com.dev404.aragomo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    double longitude;
+    double latitude;
+	TextView txtLat,txtLon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,21 +24,77 @@ public class MainActivity extends ActionBarActivity {
         
         //Creating locationManager instance
         
-        LocationManager locationManager = (LocationManager)
-        getSystemService(Context.LOCATION_SERVICE);
-        
-        //TODO: check if GPS is enabled or not
-        
-        
-        
-    }
+        /*LocationManager locationManager = (LocationManager)
+        getSystemService(Context.LOCATION_SERVICE);*/
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-    @Override
-    public void onLocationChanged(Location location) {
-	    txtLat = (TextView) findViewById(R.id.textview1);
-	    txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-	}
+        //TODO: check if GPS is enabled or not
+        // will crash if GPS is NOT enabled! 
+
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();         
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+        
+
+	    //loading webview
+	    
+	    WebView myWebView = (WebView) findViewById(R.id.webview);
+
+	    myWebView.setWebViewClient(new Callback());  //HERE IS THE MAIN CHANGE
+	    myWebView.loadUrl("http://www.google.com");
+	    
+        //refresh gps coordenates
+	    txtLat = (TextView) findViewById(R.id.gps);
+	    txtLat.setText("Latitude:" + latitude + ", Longitude:" + longitude);
+        	    
+	    
+    }
     
+    private class Callback extends WebViewClient{  //HERE IS THE MAIN CHANGE. 
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return (false);
+        }
+
+    }    
+    
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        }
+
+		@Override
+		public void onProviderDisabled(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+    };
+
+
+    //@Override
+    public void onLocationChanged(Location location) {
+        /*double longitude = location.getLongitude();
+        double latitude = location.getLatitude();*/
+	    txtLat = (TextView) findViewById(R.id.gps);
+	    txtLat.setText("Latitude:" + latitude + ", Longitude:" + longitude);
+	}
+   
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ android/Aragomo/src/com/dev404/aragomo/SplashScreenActivity.java
@@ -0,0 +1,55 @@
+package com.dev404.aragomo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+}
